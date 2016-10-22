48438f26ed3cdba28d6b1eff16e786becb7c8721
Joseph Silva <Joseph.silva.275@my.csun.edu>
2016-10-21 18:44:54 -0700

    Open Output working, output error code written

--- lab2.s
+++ lab2.s
@@ -2,6 +2,8 @@
 	.equ 	SWI_PrInt,0x6b		@ Writes an integer
 	.equ 	SWI_Timer, 0x06d	@ System Time from exection
 	.equ	SWI_PrStr, 0x69		@ Print a string
+	.equ	SWI_Open, 0x66		@ Open a file
+	.equ	SWI_Exit, 0x11		@ stop executing
 	
 	swi		SWI_Timer			@R0 has begining execution time
 			MOV R6,R0
@@ -14,6 +16,15 @@ loop:
 	SUB		R4,R4,#1			@ Decriment loop by 1
 	CMP		R4,#1				@ Break when loop is 1
 	BNE		loop				@ Branch to Loop Above
+	
+	@ ============================  Start Writing to Output  ===========================
+	
+	LDR 	R0,=OutFileName		@ set Name for output file
+	MOV 	R1,#1				@ Mode is Output	
+	swi 	SWI_Open			@ open file for output
+	bcs		OutFileError		@ if error, branch
+	LDR		R1,=OutFileHandle	@ load output file handle
+	STR		R0,[R1]				@ save the file handle
 	
 	MOV 	R0,#Stdout			@ Set mode to output
 	MOV 	R1,R3				@ Move total to R1 for output
@@ -30,9 +41,20 @@ loop:
 	MOV		R1,R3
 	swi		SWI_PrInt			@Print Integer
 	
+	@ ================================  Branches  =====================================
+	
+Exit:
+	swi		SWI_Exit			@Stop Executing
+	
+OutFileError:
+	MOV 	R0, #Stdout
+	LDR 	R1, =OutFileErrorMsg
+	swi		SWI_PrStr
+	bal		Exit				@Give up, go to end
+	
 NL:				.asciz			"\n"			@new line
 OutFileName:	.asciz 			"Outfile1.txt"
-OutFileError:	.asciz			"Unable to open output file \n"
+OutFileErrorMsg:	.asciz			"Unable to open output file \n"
 	.align
 OutFileHandle:	.word			0
 	
